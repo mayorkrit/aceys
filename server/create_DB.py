9a5b6224a971600d93af80814224d4ef58b7ff89
Igor Sadofiev <sadofid@gmail.com>
2023-02-15 18:17:14 +0300

    prod

---
+++ server/create_DB.py
@@ -0,0 +1,53 @@
+import psycopg2
+
+from psycopg2 import OperationalError
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+connection = create_connection(
+    "postgres", "postgres", "    ", "127.0.0.1", "5432"
+)
+
+def create_database(connection, query):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Query executed successfully")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+
+create_database_query = "CREATE DATABASE aceyes_users"
+create_database(connection, create_database_query)
+
+def execute_query(connection, query):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Query executed successfully")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL, 
+  email VARCHAR NOT NULL
+)
+"""
+
+execute_query(connection, create_users_table)
---
+++ server/create_tableDb.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+from psycopg2 import OperationalError
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+
+
+connection = create_connection(
+    "aceyes_users", "postgres", "    ", "127.0.0.1", "5432"
+)
+
+def execute_query(connection, query):
+    connection.autocommit = True
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Query executed successfully")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL, 
+  email VARCHAR NOT NULL
+)
+"""
+
+execute_query(connection, create_users_table)
